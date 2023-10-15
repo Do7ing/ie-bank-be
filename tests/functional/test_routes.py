60746ee26e91ae58cc943b4fb124f2c71a4861b2
Zaid Dakhqan <zdakhqan.ieu2021@student.ie.edu>
2023-10-15 15:59:10 +0200

    Updated functions

--- tests/functional/test_routes.py
+++ tests/functional/test_routes.py
@@ -29,4 +29,21 @@ def test_create_account(testing_client):
     response = testing_client.post('/accounts', json={'name': 'John Doe', 'currency': '€'})
     assert response.status_code == 200
 
+def test_get_account_details(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts/<account_number>' page is requested (GET)
+    THEN check the response is valid
+    """
+    response = testing_client.get('/accounts/1')
+    assert response.status_code == 200
 
+def test_update_account(testing_client):
+    """
+    GIVEN a Flask application
+    WHEN the '/accounts/<account_number>' page is requested (PUT)
+    THEN check the response is valid
+    """
+    response = testing_client.put(
+        '/accounts/1', json={'name': 'Updated Name', 'currency': '$', 'country': 'USA'})
+    assert response.status_code == 200
--- tests/unit/test_model.py
+++ tests/unit/test_model.py
@@ -13,3 +13,24 @@ def test_create_account():
     assert account.account_number != None
     assert account.balance == 0.0
     assert account.status == 'Active'
+    assert account.country == 'Spain'
+
+
+def test_create_account_with_initial_balance():
+    """
+    GIVEN a Account model
+    WHEN a new Account is created with an initial balance
+    THEN check the initial balance
+    """
+    account = Account('John Doe', '€', 'Spain')
+    assert account.balance == 0.0
+
+
+def test_account_status():
+    """
+    GIVEN a new Account
+    WHEN a new Account is created
+    THEN check the status is active
+    """
+    account = Account('John Doe', '€', 'Spain')
+    assert account.status == 'Active'
